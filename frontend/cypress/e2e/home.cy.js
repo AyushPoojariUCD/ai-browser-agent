b1c71c906e15897858e35a3dadbd7798bb1a8c61
PreetRaut <ayush.poojari@ucdconnect.ie>
2025-08-17 07:47:58 +0530

    test: added cypress e2e testing:frontend

---
+++ frontend/cypress/e2e/home.cy.js
@@ -0,0 +1,8 @@
+/* eslint-env cypress */
+
+describe("Home Page", () => {
+  it("renders hero section and can go to signup", () => {
+    cy.visit("/");
+    cy.contains("AI-Browser-Agent").should("exist");
+  });
+});
